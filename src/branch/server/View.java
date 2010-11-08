78fde207637416c82eaad2d71f4d0d4e642ae3ac
pracheer.gupta@gmail.com <pracheer.gupta@gmail.com@990bd6ef-21c7-f6f3-889b-de27a0ca12e2>
2010-11-08 20:32:14 +0000

    View class will contain the details of all the servers of a particular group

--- src/branch/server/View.java
+++ src/branch/server/View.java
@@ -3,14 +3,47 @@ package branch.server;
 import java.util.ArrayList;
 
 public class View {
-	
+
 	String groupId = null;
-	
+
 	ArrayList<Node> listOfServers = null;
-	
+
 	public View(String groupId, Node server) {
 		this.groupId = groupId;
 		listOfServers = new ArrayList<Node>();
 		listOfServers.add(server);
+	}
+
+	public void addServer(Node server) {
+		listOfServers.add(server);
+	}
+
+	/**
+	 * @param serverIndex: head index is 0, tail has the last index.
+	 */
+	public void removeServer(int serverIndex) {
+		listOfServers.remove(serverIndex);
+	}
+
+	public Node getHead() {
+		return listOfServers.get(0);
+	}
+
+	public Node getTail() {
+		return listOfServers.get(listOfServers.size() - 1);
+	}
+
+	public Node getPredecessor(int serverIndex) {
+		if (serverIndex == 0)
+			return null;
+		else
+			return listOfServers.get(serverIndex - 1);
+	}
+
+	public Node getSuccessor(int serverIndex) {
+		if (serverIndex == (listOfServers.size()-1))
+			return null;
+		else 
+			return listOfServers.get(serverIndex + 1);
 	}
 }
