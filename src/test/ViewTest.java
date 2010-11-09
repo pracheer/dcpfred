fccb2a3c8e788331a85fde1ab11a3e7f8bdcfd00
pracheer.gupta@gmail.com <pracheer.gupta@gmail.com@990bd6ef-21c7-f6f3-889b-de27a0ca12e2>
2010-11-09 02:04:13 +0000

    view class implemented along with its test case. oracle updated.

--- src/test/ViewTest.java
+++ src/test/ViewTest.java
@@ -6,46 +6,68 @@ import junit.framework.TestCase;
 public class ViewTest extends TestCase {
 
 	View view;
-	
+	String str;
 	public ViewTest() {
 //		view = new View(1, new Node);
 	}
 	
 	protected void setUp() throws Exception {
 		super.setUp();
-		
+		view = new View("01");
+		view.addServer("S01_01");
+		view.addServer("S01_02");
+		view.addServer("S01_03");
+		view.addServer("S01_04");
+		str = "01::S01_01::S01_02::S01_03::S01_04";
 	}
 
 	protected void tearDown() throws Exception {
 		super.tearDown();
-	}
-
-	public void testView() {
-		fail("Not yet implemented");
+		view = null;
 	}
 
 	public void testAddServer() {
-		fail("Not yet implemented");
+		view.addServer("S01_05");
+		assertEquals(str + "::S01_05", view.toString());
 	}
 
 	public void testRemoveServer() {
-		fail("Not yet implemented");
+		
+		// tail node
+		assertTrue(view.removeServer("S01_04"));
+		assertFalse(view.removeServer("S01_04"));
+		
+		// middle node
+		assertTrue(view.removeServer("S01_02"));
+		assertFalse(view.removeServer("S01_02"));
+		
+		// head node
+		assertTrue(view.removeServer("S01_01"));
+		assertFalse(view.removeServer("S01_01"));
+		
 	}
 
 	public void testGetHead() {
-		fail("Not yet implemented");
+		assertEquals("S01_01", view.getHead());
 	}
 
 	public void testGetTail() {
-		fail("Not yet implemented");
+		assertEquals("S01_04", view.getTail());
 	}
 
 	public void testGetPredecessor() {
-		fail("Not yet implemented");
+		assertEquals(null, view.getPredecessor("S01_01"));
+		assertEquals("S01_01", view.getPredecessor("S01_02"));
 	}
 
 	public void testGetSuccessor() {
-		fail("Not yet implemented");
+		assertEquals("S01_02", view.getSuccessor("S01_01"));
+		assertEquals(null, view.getSuccessor("S01_04"));
 	}
-
+	
+	public void testParseString() {
+		view = View.parseString(str);
+		assertEquals(str, view.toString());
+	}
+	
 }
