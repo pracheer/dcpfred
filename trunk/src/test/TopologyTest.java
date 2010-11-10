8e3c83b513480ee4a5dc2bd2dd5a1e01cc86f818
kejriwalnikhil@gmail.com <kejriwalnikhil@gmail.com@990bd6ef-21c7-f6f3-889b-de27a0ca12e2>
2010-11-10 12:24:54 +0000

    tests clean

--- trunk/src/test/TopologyTest.java
+++ trunk/src/test/TopologyTest.java
@@ -16,16 +16,9 @@ public class TopologyTest extends TestCase {
 		tempFile_ = File.createTempFile("topology", ".txt");
 		
 		String str = "";
-		str += "G01 S01\n";
-		str += "S01 G01\n";
-		str += "S01 S05\n";
-		str += "S02 S04\n";
-		str += "S02 S01\n";
-		str += "S02 S03\n";
+		str += "01 05\n";
+		str += "02 01\n";
 		str += "err err (topology file should ignore)\n";
-		str += "S02 G02\n";
-		str += "G02 S02\n";
-		str += "S03 S01\n";
 		
 		try {
 			FileWriter fw = new FileWriter(tempFile_);
@@ -52,36 +45,24 @@ public class TopologyTest extends TestCase {
 	}
 
 	public void testIsReachable() {
-		/**
-		 * G01 S01
-		 * S01 G01
-		 * S01 S05
-		 * S02 S04
-		 * S02 S01
-		 * S02 S03
-		 * S02 G02
-		 * G02 S02
-		 * S03 S01
-		 */
+		
 
-		Topology tpl = createTopologyFile(tempFile_.getAbsolutePath(), "S01_1", "S01");
+		Topology tpl = createTopologyFile(tempFile_.getAbsolutePath(), "S01_1", "01");
 		assertTrue(tpl.isReachable("G01"));
-		assertTrue(tpl.isReachable("S05"));
-		assertFalse(tpl.isReachable("S01"));
+		assertTrue(tpl.isReachable("S05_01"));
 
-		tpl = createTopologyFile(tempFile_.getAbsolutePath(), "G01_1", "G01");
-		assertTrue(tpl.isReachable("S01"));
-		assertFalse(tpl.isReachable("S02"));
+		tpl = createTopologyFile(tempFile_.getAbsolutePath(), "G01", "01");
+		assertTrue(tpl.isReachable("S01_01"));
+		assertTrue(tpl.isReachable("S02_01"));
 		
-		tpl = createTopologyFile(tempFile_.getAbsolutePath(), "S02_1", "S02");		
-		assertTrue(tpl.isReachable("S04"));
+		tpl = createTopologyFile(tempFile_.getAbsolutePath(), "S05_01", "05");		
+		assertTrue(tpl.isReachable("S05_02"));
 		assertTrue(tpl.isReachable("S01"));
-		assertTrue(tpl.isReachable("S03"));
-		assertTrue(tpl.isReachable("G02"));
+		assertFalse(tpl.isReachable("S03"));
+		assertFalse(tpl.isReachable("G02"));
 		
-		tpl = createTopologyFile(tempFile_.getAbsolutePath(), "G02_1", "G02");
-		assertTrue(tpl.isReachable("S02"));
-		assertFalse(tpl.isReachable("S01"));
+		tpl = createTopologyFile(tempFile_.getAbsolutePath(), "G02", "02");
+		assertTrue(tpl.isReachable("S02_01"));
 	}
 	
 	public void testTopologyCreation() {
@@ -96,36 +77,4 @@ public class TopologyTest extends TestCase {
 		}
 	}
 	
-	public void testWhoNeighbors() {
-		/**
-		 * G01 S01
-		 * S01 G01
-		 * S01 S05
-		 * S02 S04
-		 * S02 S01
-		 * S02 S03
-		 * S02 G02
-		 * G02 S02
-		 */
-		
-		Topology tpl = null;
-		
-		ArrayList<String> expectedNeighbors,neighbors;
-		expectedNeighbors = new ArrayList<String>();
-
-		tpl = createTopologyFile(tempFile_.getAbsolutePath(), "S02_1", "S02");
-		expectedNeighbors.clear();
-		expectedNeighbors.add("S04");
-		expectedNeighbors.add("S01");
-		expectedNeighbors.add("S03");
-		neighbors = tpl.getOutNeighbors();
-		assertTrue(expectedNeighbors.equals(neighbors));
-		
-		tpl = createTopologyFile(tempFile_.getAbsolutePath(), "S01_1", "S01");
-		expectedNeighbors.clear();
-		expectedNeighbors.add("S02");
-		expectedNeighbors.add("S03");
-		neighbors = tpl.getInNeighbors();
-		assertTrue(expectedNeighbors.equals(neighbors));
-	}
 }
